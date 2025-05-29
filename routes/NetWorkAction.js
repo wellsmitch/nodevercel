6c117c0e10ee216f92413b98c4e687a04182736f
songyubnin <123@example.com>
2025-05-29 10:16:39 +0800

    d

--- routes/NetWorkAction.js
+++ routes/NetWorkAction.js
@@ -1,29 +1,29 @@
 const axios = require("axios")
-console.log('axios',axios)
+console.log('axios', axios)
 // const CryptoJS = require("crypto-js")
 const { leancloudApi, appId, appKey, marsterKey } = require("./constant.js")
 
 
 const NetWorkAction = axios.create({
- baseURL: leancloudApi,
+  baseURL: leancloudApi,
 });
 
 NetWorkAction.interceptors.request.use((config) => {
 
-   const md5Fn = (str) => CryptoJS.MD5(str)
-   
- const dd = Date.now();
- const tempInfo = !config.headers.autoWell ? { "X-LC-Sign": `${md5Fn(dd + appKey)},${dd}` }
-  : { "X-LC-key": `${md5Fn(dd + marsterKey)},${dd},master` }
-  console.log('tempInfo',tempInfo)
-//  config.headers = {
-//   ...config.headers,
-//   ...tempInfo,
-//   "X-LC-Id": appId,
-//  }
- return config
+  const md5Fn = (str) => CryptoJS.MD5(str)
+
+  const dd = Date.now();
+  const tempInfo = !config.headers.autoWell ? { "X-LC-Sign": `${md5Fn(dd + appKey)},${dd}` }
+    : { "X-LC-key": `${md5Fn(dd + marsterKey)},${dd},master` }
+  console.log('tempInfo', tempInfo)
+  config.headers = {
+    ...config.headers,
+    ...tempInfo,
+    "X-LC-Id": appId,
+  }
+  return config
 }, (err) => {
- return Promise.reject(err)
+  return Promise.reject(err)
 })
 
 
