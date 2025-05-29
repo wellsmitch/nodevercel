19eae4afbff604404bb7afe713df22b5a7196e8e
songyubnin <123@example.com>
2025-05-29 15:26:10 +0800

    s

--- routes/onelineUser.js
+++ routes/onelineUser.js
@@ -26,7 +26,7 @@ router.post("/login", multer().any(), async (req, res) => {
  try {
   const netRes = await queryUser(account, password)
 
-  res.send(new Result({ ...netRes, message: netRes.data.results.length > 0 ? "登录成功" : "用户不存在" }).getResult())
+  res.send(new Result({ ...netRes, message: netRes?.data?.results?.length > 0 ? "登录成功" : "用户不存在" }).getResult())
 
   return
  } catch (errr) {
@@ -35,28 +35,28 @@ router.post("/login", multer().any(), async (req, res) => {
 })
 
 
-// router.post("/regeister", multer().any(), async (req, res) => {
-//  const { account, password } = req.body
-//  try {
-//   const netRes = await queryUser(account, password)
-//   if (netRes?.data?.results?.length > 0) {
-//    res.send({
-//     message: "用户已存在"
-//    })
-//    return
-//   }
+router.post("/register", multer().any(), async (req, res) => {
+ const { account, password } = req.body
+ try {
+  const netRes = await queryUser(account, password)
+  if (netRes?.data?.results?.length > 0) {
+   res.send({
+    message: "用户已存在"
+   })
+   return
+  }
 
-//   const netResRegeister = await NetWorkAction.post("/OnlineUser", {
-//    account,
-//    password
-//   })
+  const netResRegeister = await NetWorkAction.post("/OnlineUser", {
+   account,
+   password
+  })
 
-//   res.send(new Result({ ...netResRegeister, message: "注册成功" }).getResult())
-//   return
-//  } catch (errr) {
-//   res.send(new Result({ message: "注册失败" }).getResult())
-//  }
-// })
+  res.send(new Result({ ...netResRegeister, message: "注册成功" }).getResult())
+  return
+ } catch (errr) {
+  res.send(new Result({ message: "注册失败" }).getResult())
+ }
+})
 
 
 module.exports = router
