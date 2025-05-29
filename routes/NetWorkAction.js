cda4823c475f105786d1dcfcbd13ba5cec07c613
songyubnin <123@example.com>
2025-05-29 10:06:39 +0800

    d

--- routes/NetWorkAction.js
+++ routes/NetWorkAction.js
@@ -8,20 +8,20 @@ const NetWorkAction = axios.create({
  baseURL: leancloudApi,
 });
 
-NetWorkAction.interceptors.request.use((config) => {
-   const md5Fn = (str) => CryptoJS.MD5(str)
- const dd = Date.now();
- const tempInfo = !config?.headers?.autoWell ? { "X-LC-Sign": `${md5Fn(dd + appKey)},${dd}` }
-  : { "X-LC-key": `${md5Fn(dd + marsterKey)},${dd},master` }
- config.headers = {
-  ...config.headers,
-  ...tempInfo,
-  "X-LC-Id": appId,
- }
- return config
-}, (err) => {
- return Promise.reject(err)
-})
+// NetWorkAction.interceptors.request.use((config) => {
+//    const md5Fn = (str) => CryptoJS.MD5(str)
+//  const dd = Date.now();
+//  const tempInfo = !config?.headers?.autoWell ? { "X-LC-Sign": `${md5Fn(dd + appKey)},${dd}` }
+//   : { "X-LC-key": `${md5Fn(dd + marsterKey)},${dd},master` }
+//  config.headers = {
+//   ...config.headers,
+//   ...tempInfo,
+//   "X-LC-Id": appId,
+//  }
+//  return config
+// }, (err) => {
+//  return Promise.reject(err)
+// })
 
 
 module.exports = NetWorkAction
