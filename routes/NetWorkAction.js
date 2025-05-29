92fd4faa87e487f09512ea99d2b47d2d14d8c66d
songyubnin <123@example.com>
2025-05-29 10:15:56 +0800

    d

--- routes/NetWorkAction.js
+++ routes/NetWorkAction.js
@@ -13,7 +13,7 @@ NetWorkAction.interceptors.request.use((config) => {
    const md5Fn = (str) => CryptoJS.MD5(str)
    
  const dd = Date.now();
- const tempInfo = !config?.headers?.autoWell ? { "X-LC-Sign": `${md5Fn(dd + appKey)},${dd}` }
+ const tempInfo = !config.headers.autoWell ? { "X-LC-Sign": `${md5Fn(dd + appKey)},${dd}` }
   : { "X-LC-key": `${md5Fn(dd + marsterKey)},${dd},master` }
   console.log('tempInfo',tempInfo)
 //  config.headers = {
