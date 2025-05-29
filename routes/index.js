a516e8de5b506936f0f72d5a6e159babb2a7b3c2
songyubnin <123@example.com>
2025-05-29 09:07:10 +0800

    p

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,67 @@
 const router = require("./createRouter")
 
-require("./OnlineUser/index.js")
+// require("./OnlineUser/index.js")
+
+var express = require('express');
+var dotenv = require('dotenv');
+const CryptoJS = require("crypto-js")
+// const dayjs = require("dayjs")
+const multer = require('multer')
+const NetWorkAction = require("./NetWorkAction")
+const Result = require("../utils/Result")
+console.log('express',express)
+console.log('dotenv',dotenv)
+console.log('dayjs',dayjs)
+console.log('CryptoJS',CryptoJS)
+
+const queryUser = async (account, password) => {
+ const netRes = await NetWorkAction.get("/OnlineUser", {
+  params: {
+   where: {
+    account,
+    password
+   }
+  }
+ })
+ return netRes
+}
+
+router.post("/login", multer().any(), async (req, res) => {
+ const { account, password } = req.body
+ try {
+  const netRes = await queryUser(account, password)
+
+  res.send(new Result({ ...netRes, message: netRes.data?.results?.length > 0 ? "登录成功" : "用户不存在" }).getResult())
+
+  return
+ } catch (errr) {
+  res.send(new Result({ message: "登录失败" }).getResult())
+ }
+})
+
+
+router.post("/regeister", multer().any(), async (req, res) => {
+ const { account, password } = req.body
+ try {
+  const netRes = await queryUser(account, password)
+  if (netRes?.data?.results?.length > 0) {
+   res.send({
+    message: "用户已存在"
+   })
+   return
+  }
+
+  const netResRegeister = await NetWorkAction.post("/OnlineUser", {
+   account,
+   password
+  })
+
+  res.send(new Result({ ...netResRegeister, message: "注册成功" }).getResult())
+  return
+ } catch (errr) {
+  res.send(new Result({ message: "注册失败" }).getResult())
+ }
+})
 
 
 
