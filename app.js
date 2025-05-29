cfbc83f4aee77378b9341bc16b36a2b44dc62e0a
songyubnin <123@example.com>
2025-05-29 16:03:19 +0800

    d

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use("/downloads", express.static(path.join(__dirname, 'files')));
-app.use("/", express.static(path.join(__dirname, 'public')));
+// app.use("/", express.static(path.join(__dirname, 'public')));
 
 // app.use(bodyParser.urlencoded({extended: true})); //创建 application/x-www-form-urlencoded 解析
 // app.use(bodyParser.json());
