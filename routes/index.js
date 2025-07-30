cd6cac56fc4ccf0f656c5ea367dbad8db037887f
songyubnin <123@example.com>
2025-07-30 14:49:24 +0800

    qq& yzm

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,8 @@
 const router = require("./createRouter.js") 
 
-// require("./onelineUser.js") 
-// require("./qqrouter.js") 
+require("./onelineUser.js") 
+require("./qqrouter.js") 
 require("./leanCloud.js") 
-// require("./article.js") 
-// require("./category.js") 
-// require("./role.js") 
-// require("./userList.js") 
-// require("./x6List.js") 
 
 
 module.exports = router
